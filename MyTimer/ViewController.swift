90b7adb5d5d605c4eb12bf783cea5cd9da60e2e9
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-06-21 14:44:12 +0900

    ViewControllerと関連付け

--- MyTimer/ViewController.swift
+++ MyTimer/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countDownLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func settingButtonAction(_ sender: Any) {
+    }
+
+    @IBAction func startButtonAction(_ sender: Any) {
+    }
+
+    @IBAction func stopButtonAction(_ sender: Any) {
+    }
 
 }
 
