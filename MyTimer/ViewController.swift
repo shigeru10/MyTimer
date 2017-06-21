1a05616238d2c7733f19c659a8b3b2184bec0570
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-06-21 13:45:42 +0900

    Initial Commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by SuzukiShigeru on 2017/06/21.
+//  Copyright © 2017年 Shigeru Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
